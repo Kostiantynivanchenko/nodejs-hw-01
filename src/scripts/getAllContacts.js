3b88e64b74410baad10c2e1cf9634340b28517c4
kostiantyn_ivanchenko <kokos2702@gmail.com>
2025-04-10 12:48:24 +0200

    first commit

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const data = await readContacts();
+    return data;
+  } catch (err) {
+    console.error('Помилка читання файлу:', err);
+  }
+};
 
 console.log(await getAllContacts());
